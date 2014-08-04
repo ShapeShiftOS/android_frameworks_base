1b8548de10cf433b81f4398caea92fe0851d2083
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Keyguard: Add option to scramble pin layout when unlocking (1/2)
    Squashed:
    Keyguard: Don't disable visibility when scrambling pin
    Keyguard: Fix scramblepin logic
    
    Change-Id: I6b3763c05f8dc6d49c62c5dc5c01dce8d18d09ca
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3934,6 +3934,13 @@ public final class Settings {
             "lock_pattern_tactile_feedback_enabled";
 
         /**
+         * Whether to scramble a pin unlock layout
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT =
+            "lockscreen_scramble_pin_layout";
+
+        /**
          * A formatted string of the next alarm that is set, or the empty string
          * if there is no alarm set.
          *
@@ -4986,6 +4993,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(EGG_MODE);
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_PIN_SCRAMBLE_LAYOUT);
         }
 
         /**
