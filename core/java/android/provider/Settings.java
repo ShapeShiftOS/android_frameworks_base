d7cc508a8d4e7ddfc0c86f347e471702ee361c32
htchoi1995 <htchoi1995@gmail.com>
2014-11-12 14:07:51 -0500

    Disable/Enable screenshot sound [1/2]
    @ neobuddy89: Adapted for Android 11
    
    Change-Id: I9f32d0026833f9350a2e8e4bedaff3801f508e76
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5540,6 +5540,12 @@ public final class Settings {
 
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
