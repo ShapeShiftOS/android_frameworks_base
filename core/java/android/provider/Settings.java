c9ce1d6de256739152351e4fb527bc9ed7f855d9
maxwen <max.weninger@gmail.com>
2017-08-27 18:37:00 -0700

    base: Global Actions with Advanced Reboot [1/2]
    With fixes from:
    
    Do not show reboot dialog in lock screen
    -Adin Kwok <adin.kwok@carbonrom.org>
    
    Dismiss dialog on screen off
    -Adin Kwok <adin.kwok@carbonrom.org>
    
    Vectorized the power menu sound panel selector
    -David Wilson <daveyannihilation@hotmail.com
    
    Fix silent mode icons in new power menu
    Fix airplane mode in new power menu
    -camcory <camcory1@gmail.com>
    
    Pimp out power menu
    -Alex Cruz <mazdarider23@gmail.com>
    
    Powermenu Torch option
    -LorDClockaN <davor@losinj.com>
    
    Add Screen Record to the Power Menu
    -eyosen <abittin@gmail.com>
    
    Power menu customizations [1/2]
    -Dave Kessler <activethrasher00@gmail.com>
    
    [anayw2001 edits: bring up to 11]
    
    Change-Id: I6978be623b0ec7ca9e702fc311449c36b504b1a6
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    (cherry picked from commit 7a858810dc2c2984457a17c4d2385c3b8c0123ab)
    
    base: fix controls UI appearing on advanced reboot menu
    
    Change-Id: Iac58f7422bda51fe06a4a96f5e2d3fd99660f3af

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5285,6 +5285,11 @@ public final class Settings {
         public static final String FOD_GESTURE = "fod_gesture";
 
         /**
+         * @hide
+         */
+        public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -9233,6 +9238,11 @@ public final class Settings {
          * @hide
          */
         public static final String NOTIFICATION_DISMISS_RTL = "notification_dismiss_rtl";
+
+        /**
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
          * Comma separated list of QS tiles that have been auto-added already.
