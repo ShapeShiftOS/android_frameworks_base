fe6a2fa8ba48d1f0035048b22cd02c1bd1f4aa07
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-13 16:26:50 -0300

    SettingsLib: Don't show system overlays on apps list
    Change-Id: I7e45d0ee59bd973245ce5e6b7c54ad0ad0e634f2

--- packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
+++ packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
@@ -70,6 +70,7 @@ import java.lang.ref.WeakReference;
 import java.text.Collator;
 import java.text.Normalizer;
 import java.text.Normalizer.Form;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -79,6 +80,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.regex.Pattern;
+
+import com.android.internal.util.ssos.OverlayUtils;
 
 /**
  * Keeps track of information about all installed applications, lazy-loading
@@ -1869,13 +1872,14 @@ public class ApplicationsState {
     };
 
     public static final AppFilter FILTER_EVERYTHING = new AppFilter() {
+
         @Override
         public void init() {
         }
 
         @Override
         public boolean filterApp(AppEntry entry) {
-            return true;
+            return !Arrays.asList(OverlayUtils.AllPackages).contains(entry.info.packageName);
         }
     };
 
