40b56c67b827bcd1654e502ba88be72fd94ffddb
Pranav Vashi <neobuddy89@gmail.com>
2019-07-26 12:45:22 +0300

    Less boring heads up option: always show alarm clock headsup
    AshutoshSundresh: Document change in Settings
    
    Change-Id: Iea1f6c7a131689eed3d6bee8b28bebd91a56c412
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5415,7 +5415,7 @@ public final class Settings {
         public static final String POCKET_JUDGE = "pocket_judge";
 
         /**
-         * Whether to show heads up only for dialer and sms apps
+         * Whether to show heads up only for dialer, clock and sms apps
          * @hide
          */
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
