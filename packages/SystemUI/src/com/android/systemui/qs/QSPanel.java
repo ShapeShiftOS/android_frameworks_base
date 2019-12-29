e4ae11bd637e66b8a69d81a3f6a74229ade304bf
00day0 <therandomuser11@gmail.com>
2019-12-29 16:33:44 +0100

    Get rid of divider in QS Why google? why? Its completly pointless to have invisible divider here By default Tile.STATE_ACTIVE is same color as QS background, since https://github.com/Extended-UI/android_frameworks_base/commit/799799ad291a2dd5a4fb37928b63058936bb3b26 it became colorAccent so divider is showing up again lets get rid of it
    Signed-off-by: 00day0 <therandomuser11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -101,7 +101,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private Record mDetailRecord;
 
     private BrightnessMirrorController mBrightnessMirrorController;
-    private View mDivider;
 
     public QSPanel(Context context) {
         this(context, null);
@@ -131,8 +130,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         mQsTileRevealController = new QSTileRevealController(mContext, this,
                 (PagedTileLayout) mTileLayout);
 
-        addDivider();
-
         mFooter = new QSSecurityFooter(this, context);
         addView(mFooter.getView());
 
@@ -148,13 +145,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
         mIsAutomaticBrightnessAvailable = getResources().getBoolean(
                 com.android.internal.R.bool.config_automatic_brightness_available);
-    }
-
-    protected void addDivider() {
-        mDivider = LayoutInflater.from(mContext).inflate(R.layout.qs_divider, this, false);
-        mDivider.setBackgroundColor(Utils.applyAlpha(mDivider.getAlpha(),
-                getColorForState(mContext, Tile.STATE_ACTIVE)));
-        addView(mDivider);
     }
 
     @Override
@@ -176,7 +166,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     }
 
     public View getDivider() {
-        return mDivider;
+        return LayoutInflater.from(mContext).inflate(R.layout.qs_divider, this, false);
     }
 
     public QSTileRevealController getQsTileRevealController() {
