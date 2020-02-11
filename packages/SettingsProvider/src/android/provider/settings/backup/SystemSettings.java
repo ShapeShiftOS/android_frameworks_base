c47eed18af36e8d7ea7e4cb3f341e06d0f5eec30
maxwen <max.weninger@gmail.com>
2020-02-11 13:31:04 +0100

    base: Cleanup notification pulse values
    a few unneded and a few with wrong names
    
    Change-Id: I755282a0a7d7d7d1f556d10ac8fbb8af05447909

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -87,9 +87,9 @@ public class SystemSettings {
         Settings.System.DOZE_ON_CHARGE,
         Settings.System.LOCKSCREEN_MEDIA_METADATA,
         Settings.System.VOLUME_ROCKER_WAKE,
-        Settings.System.PULSE_AMBIENT_LIGHT,
-        Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED,
-        Settings.System.AMBIENT_NOTIFICATION_LIGHT,
-        Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACCENT,
+        Settings.System.NOTIFICATION_PULSE,
+        Settings.System.AOD_NOTIFICATION_PULSE,
+        Settings.System.AOD_NOTIFICATION_PULSE_CLEAR,
+        Settings.System.NOTIFICATION_PULSE_ACCENT,
     };
 }
