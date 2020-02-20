f1f88c840988cbff39dceb5276889eafa1532e1f
DennySPB <dennyspb@gmail.com>
2020-02-20 18:52:57 +0200

    SystemUI: add optional back gesture vibration tick [1/2]
    Change-Id: Ibeb3850a427605ecfe203acf8a00b8b895cb2646

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4997,6 +4997,9 @@ public final class Settings {
          */
         public static final String QS_COLUMNS_LANDSCAPE = "qs_columns_landscape";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
