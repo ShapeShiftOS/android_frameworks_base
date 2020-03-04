5a30bb2bc333ef60e2cfe1c609857b621f942010
00day0 <therandomuser11@gmail.com>
2020-03-04 23:21:42 +0100

    blur: increase radius, remove it in landscape && improve animation
    Signed-off-by: 00day0 <therandomuser11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -83,6 +83,7 @@ import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.media.AudioAttributes;
 import android.metrics.LogMaker;
@@ -1144,7 +1145,8 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     public void updateBlurVisibility() {
 
-        int QSBlurAlpha = Math.round(255.0f * mStaticNotificationPanel.getExpandedFraction());
+        int QSBlurAlpha = Math.round(255.0f * (mNotificationPanel.getExpandedHeight() / (getDisplayHeight() * 0.4f)));
+        if (QSBlurAlpha > 255) QSBlurAlpha = 255;
 
         if (QSBlurAlpha > 0 && !dataupdated && !mIsKeyguard) {
             DataUsageView.updateUsage();
@@ -2891,6 +2893,14 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         mViewHierarchyManager.updateRowStates();
         mScreenPinningRequest.onConfigurationChanged();
+
+        if(mQSBlurView != null) {
+            int orientation = mContext.getResources().getConfiguration().orientation;
+            if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                Drawable emptyDrawable = new ColorDrawable(Color.TRANSPARENT);
+                mQSBlurView.setBackgroundDrawable(emptyDrawable);
+            }
+        }
     }
 
     @Override
