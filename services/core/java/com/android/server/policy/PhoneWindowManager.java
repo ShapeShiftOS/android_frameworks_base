d742dac8b1cd911a8449180856a317b33f02ca2a
Anushek Prasal <anushekprasal@gmail.com>
2020-04-14 20:25:00 +0530

    base: Disable DT2D when Screen Off FOD is enabled

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -421,6 +421,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mDoubleTapToDoze;
     private boolean mNativeDoubleTapToDozeAvailable;
 
+    private boolean mFODEnabled;
+
     // Assigned on main thread, accessed on UI thread
     volatile VrManagerInternal mVrManagerInternal;
 
@@ -2379,6 +2381,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 Settings.Secure.DOUBLE_TAP_TO_WAKE, 0) == 1;
         mDoubleTapToDoze = Settings.System.getInt(resolver,
                 Settings.System.DOZE_TRIGGER_DOUBLETAP, 0) == 1;
+
+        mFODEnabled = Settings.System.getInt(resolver,
+                Settings.System.SCREEN_OFF_FOD, 0) == 1;
 
         synchronized (mLock) {
             mHasNavigationBar = NavbarUtils.isEnabled(mContext);
@@ -4654,7 +4659,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_WAKEUP: {
                 result &= ~ACTION_PASS_TO_USER;
                 // Double-tap-to-doze
-                if (mDoubleTapToWake && mDoubleTapToDoze && !mNativeDoubleTapToDozeAvailable) {
+                if (mDoubleTapToWake && mDoubleTapToDoze && !mNativeDoubleTapToDozeAvailable && !mFODEnabled) {
                     isWakeKey = false;
                     if (!down) {
                         mContext.sendBroadcast(new Intent("com.android.systemui.doze.pulse"));
