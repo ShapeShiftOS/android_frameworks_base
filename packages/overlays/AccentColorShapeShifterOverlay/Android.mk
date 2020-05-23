0f9f71ab8cfb7742d324873449b74c6976a78fcc
AshutoshSundresh <62063875+AshutoshSundresh@users.noreply.github.com>
2020-05-23 20:55:54 +0530

    fwb: Fix derp

--- packages/overlays/AccentColorShapeShifterOverlay/Android.mk
+++ packages/overlays/AccentColorShapeShifterOverlay/Android.mk
@@ -25,7 +25,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-LOCAL_PACKAGE_NAME := AccentColorShapeShifter
+LOCAL_PACKAGE_NAME := AccentColorShapeShifterOverlay
 LOCAL_SDK_VERSION := current
 
 include $(BUILD_RRO_PACKAGE)
