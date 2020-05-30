2c8d1ef5b92fa5626fa126ec0df7e997c348db79
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-05-30 10:08:34 +0000

    [SQUASH] Revert Previous Network Traffic Indicator
    Broken
    
    This reverts commit f382aeb87a84d136b9e02932180307cf65ce9ff1.
    
    Revert "TraficSB: Refactor text formatting"
    
    This reverts commit 257ca6a6385bfe001244afae34ae951313b2a6b4.
    
    Revert "TrafficSB: Pass the animate bool"
    
    This reverts commit d09c494184a500c02a8bc567d4f8bff432623ddf.
    
    Revert "TrafficSB: fix the icon overlap on notched devices"
    
    This reverts commit d047a20fa1eb51fd802173bc001f4ddecbf50bf0.
    
    Revert "Network traffic: Retain our notch check"
    
    This reverts commit 506432e842b50867f11f74c31208cc5f25555ef6.
    
    Revert "TrafficSB: Hide arrows in statusbar traffic indicators [1/2]"
    
    This reverts commit a17b81e5dc0e2eac929d075338b4debaee9e6597.
    
    Revert "TrafficSB: dynamic indicators for statusbar network traffic"
    
    This reverts commit 82e5315aabf68a8984480da89516dfda19170d3f.
    
    Revert "NetworkTrafficSB: OOS style network traffic indicator"
    
    This reverts commit 0ed1257e6c223bd15263bf01380b0cd7c1c9a55d.
    
    Revert "Add network traffic monitor properly to statusbar icons"
    
    This reverts commit c113a0c5de58306201ce9e653529fccae018a54b.
    
    Revert "Bring back traffic indicator for all!"
    
    This reverts commit 02c00fb063b3dd83e18dbc8328f142bc93e62042.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4909,24 +4909,6 @@ public final class Settings {
         private static final Validator QS_LAYOUT_COLUMNS_VALIDATOR =
                 ANY_INTEGER_VALIDATOR;
 
-        /**
-         * Wheter to show network traffic indicator in statusbar
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
-
-        /**
-         * Network traffic inactivity threshold (default is 1 kBs)
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
-
-        /**
-         * Whether to disable showing arrows in statusbar network traffic indicators
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
-
         /** @hide */
         public static final String GESTURE_PILL_TOGGLE = "gesture_pill_toggle";
 
