39d523116c144cc3092b0980d29b2bfbf1c56e71
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-27 19:05:42 +0530

    DisplayPolicy: Fix watchdog when adding new display [2]

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -633,6 +633,9 @@ public class DisplayPolicy {
             } else if ("0".equals(navBarOverride)) {
                 mHasNavigationBar = true;
             }
+
+            // Register content observer only for main display
+            mSettingsObserver = new SettingsObserver(mHandler);
         } else {
             mHasStatusBar = false;
             mHasNavigationBar = mDisplayContent.supportsSystemDecorations();
@@ -642,9 +645,6 @@ public class DisplayPolicy {
                 mDisplayContent.getDisplayInfo(),
                 mService.mHighRefreshRateBlacklist);
 
-        if (mDisplayContent.isDefaultDisplay) {
-            mSettingsObserver = new SettingsObserver(mHandler);
-        }
     }
 
     void systemReady() {
