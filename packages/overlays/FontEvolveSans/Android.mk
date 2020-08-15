5f831b4b6d22604a393c6192e839940f4c37c986
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-08-15 19:55:53 +0530

    overlays: Move new fonts to where they belong

--- packages/overlays/FontEvolveSans/Android.mk
+++ packages/overlays/FontEvolveSans/Android.mk
@@ -18,9 +18,13 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_RRO_THEME := FontEvolveSans
+
 LOCAL_PRODUCT_MODULE := true
+
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
 LOCAL_PACKAGE_NAME := FontEvolveSans
 LOCAL_SDK_VERSION := current
 
