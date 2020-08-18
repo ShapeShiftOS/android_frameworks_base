c5303e0b74418c2dfe46cd890debcfe9ed70616b
Alexandru Scurtu <sasha.scurtu@gmail.com>
2020-08-18 14:59:16 +0300

    fwb: Add new FODAnims to backup

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5232,7 +5232,7 @@ public final class Settings {
         public static final String FOD_ANIM = "fod_anim";
         /** @hide */
         private static final Validator FOD_ANIM_VALIDATOR =
-               new SettingsValidators.InclusiveIntegerRangeValidator(0, 22);
+               new SettingsValidators.InclusiveIntegerRangeValidator(0, 24);
 
         /**
          * QS blur alpha
