1925334f53dde430f0a10050188a09f00a477715
Raaj52 <mail2raaj52@gmail.com>
2020-08-22 17:58:46 +0300

    fwb: Add Mi 10 Ultra FOD animations to backup

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5250,7 +5250,7 @@ public final class Settings {
         public static final String FOD_ANIM = "fod_anim";
         /** @hide */
         private static final Validator FOD_ANIM_VALIDATOR =
-               new SettingsValidators.InclusiveIntegerRangeValidator(0, 25);
+               new SettingsValidators.InclusiveIntegerRangeValidator(0, 27);
 
         /**
          * QS blur alpha
