5fd7014785da85b0597618b011821eb7f90a22e0
Alex <du.alexcruz@gmail.com>
2020-09-25 18:23:31 +0530

    Utils: Add method to determine battery health
    Also includes:
    
    Utils: Introduce method to determine countries that use Fahrenheit
    
    This one is a weird one because up until a few days ago, I didn't think
    I would need. Basically what this does is check the device's MCC and bounce
    it off an array of MCC's that belong to countries that use fahrenheit.
    
    - If no MCC is found (whether is a WiFi only tablet or the user has their phone
    in airplane mode), the method returns false (Celsius). Celsius is used by 99 %
    of all people so is not a bad option.
    
    Inspired by Henrique Silva <jhenrique09.mcz@hotmail.com> and his commit below
    https://gerrit.pixelexperience.org/#/c/frameworks_base/+/1606/
    
    We're going to end up using this for two things, our battery temp thingy in
    Settings and also for what Henrique used it for, the keyguard weather default.

--- core/java/com/android/internal/util/exui/Utils.java
+++ core/java/com/android/internal/util/exui/Utils.java
@@ -19,6 +19,7 @@ package com.android.internal.util.exui;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.om.IOverlayManager;
 import android.content.om.OverlayInfo;
 import android.content.pm.PackageInfo;
@@ -29,6 +30,7 @@ import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.input.InputManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
@@ -44,6 +46,8 @@ import android.view.IWindowManager;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
@@ -54,6 +58,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -132,6 +137,34 @@ public class Utils {
     // Check to see if device supports Bluetooth
     public static boolean hasBluetooth(Context context) {
         return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
+    }
+
+    // Method to detect battery temperature
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32:c);
+    }
+
+    // Method to detect countries that use Fahrenheit
+    public static boolean mccCheck(Context context) {
+        // MCC's belonging to countries that use Fahrenheit
+        String[] mcc = {"364", "552", "702", "346", "550", "376", "330",
+                "310", "311", "312", "551"};
+        TelephonyManager tel = (TelephonyManager) context.getSystemService(
+                Context.TELEPHONY_SERVICE);
+        String networkOperator = tel.getNetworkOperator();
+        // Check the array to determine celsius or fahrenheit.
+        // Default to celsius if can't access MCC
+        return !TextUtils.isEmpty(networkOperator) && Arrays.asList(mcc).contains(
+                networkOperator.substring(0, /*Filter only 3 digits*/ 3));
     }
 
     // Check to see if device supports an alterative ambient display package
