08da49b0b4ecff40cd6d59ef54a5a232ff4cede6
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-10-16 16:06:50 +0530

    FODCircleView: Use RemoveViewImmediate

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -906,7 +906,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
             mPressedParams.screenBrightness = 0.0f;
             mPressedParams.dimAmount = 0.0f;
             if (mPressedView.getParent() != null) {
-                mWindowManager.removeView(mPressedView);
+                mWindowManager.removeViewImmediate(mPressedView);
             }
             updateIconDim(true);
         }
@@ -1117,7 +1117,7 @@ class FODAnimation extends ImageView {
                 recognizingAnim.selectDrawable(0);
             }
             if (this.getWindowToken() != null) {
-                mWindowManager.removeView(this);
+                mWindowManager.removeViewImmediate(this);
             }
         }
     }
