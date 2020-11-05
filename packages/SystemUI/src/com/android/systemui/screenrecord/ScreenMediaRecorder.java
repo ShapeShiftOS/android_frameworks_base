5bf2a7a033baa1a896efafec281232632e958234
Arne Coucheron <arco68@gmail.com>
2020-11-05 05:38:44 +0100

    SystemUI: Use AVCProfileMain for screen recorder
    Some devices don't support the High profile very well.
    Use Main profile, which is more compatible, so they
    can use screen recorder too.
    
    Change-Id: Iac23480e080edf4300e6f411c3394c0b41030daa

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -144,9 +144,9 @@ public class ScreenMediaRecorder {
         but at the same time the cpu load is 8-10 times higher and some devices don't support it yet */
         mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
         mMediaRecorder.setVideoEncodingProfileLevel(
-                MediaCodecInfo.CodecProfileLevel.AVCProfileHigh,/*actually the common used high level profile*/
-                mLowQuality ? MediaCodecInfo.CodecProfileLevel.AVCLevel2/*level 3.2*/
-                : MediaCodecInfo.CodecProfileLevel.AVCLevel3/*level 4.2*/);
+                MediaCodecInfo.CodecProfileLevel.AVCProfileMain,
+                mLowQuality ? MediaCodecInfo.CodecProfileLevel.AVCLevel3
+                : MediaCodecInfo.CodecProfileLevel.AVCLevel42/*level 4.2*/);
         mMediaRecorder.setVideoSize(screenWidth, screenHeight);
         mMediaRecorder.setVideoFrameRate(refereshRate);
         mMediaRecorder.setVideoEncodingBitRate(vidBitRate);
