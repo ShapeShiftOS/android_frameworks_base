01330dfd6c212174cf8daef3783484bf230e9d55
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-12-08 22:32:56 +0530

    Revert "Recolor assistant animation to SSOS colors"
    New Google Assistant update makes this look pretty bad

--- packages/SystemUI/src/com/android/systemui/assist/ui/InvocationLightsView.java
+++ packages/SystemUI/src/com/android/systemui/assist/ui/InvocationLightsView.java
@@ -117,9 +117,11 @@ public class InvocationLightsView extends View
         }
 
         Resources res = mContext.getResources();
-        int colorDark = res.getColor(R.color.edge_light_dark);
-        int colorDim = res.getColor(R.color.edge_light_dim);
-        setColors(colorDark, colorDim, colorDark, colorDim);
+        int colorRed = res.getColor(R.color.edge_light_red);
+        int colorYellow = res.getColor(R.color.edge_light_yellow);
+        int colorBlue = res.getColor(R.color.edge_light_blue);
+        int colorGreen = res.getColor(R.color.edge_light_green);
+        setColors(colorBlue, colorRed, colorYellow, colorGreen);
     }
 
     /**
