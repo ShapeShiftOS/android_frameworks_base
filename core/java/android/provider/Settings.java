9a33d808e8deee306c3a58adb2e9c27285ca35c8
TikkiTikki <radstylemusic@gmail.com>
2021-02-03 21:43:34 -0300

    base: Replace media notification background with artwork [1/2]
    Why not?

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5857,6 +5857,12 @@ public final class Settings {
         public static final String SHOW_FPS_OVERLAY = "show_fps_overlay";
 
         /**
+         * Enable and disable Artwork on background media notification
+         * @hide
+         */
+        public static final String ARTWORK_MEDIA_BACKGROUND = "artwork_media_background";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
