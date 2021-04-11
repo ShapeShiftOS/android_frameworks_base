36c87e3d7a6475c5d5b6df3bcc0b5b3df627b215
Alexandru Scurtu <sasha.scurtu@gmail.com>
2021-04-11 12:08:36 +0300

    Revert "base: Replace media notification background with artwork [1/2]"
    broken
    
    This reverts commit 9a33d808e8deee306c3a58adb2e9c27285ca35c8.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5857,12 +5857,6 @@ public final class Settings {
         public static final String SHOW_FPS_OVERLAY = "show_fps_overlay";
 
         /**
-         * Enable and disable Artwork on background media notification
-         * @hide
-         */
-        public static final String ARTWORK_MEDIA_BACKGROUND = "artwork_media_background";
-
-        /**
          * Whether or not to vibrate when a touchscreen gesture is detected
          * @hide
          */
