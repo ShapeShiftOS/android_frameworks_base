b600e3fefde751dbd2b312e34fe66cf4fe74b4f8
Alexandru Scurtu <sasha.scurtu@gmail.com>
2021-05-03 11:14:52 +0300

    base: Move CutoutUtils to custom

--- core/java/com/android/internal/util/custom/cutout/CutoutUtils.java
+++ core/java/com/android/internal/util/custom/cutout/CutoutUtils.java
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.android.internal.util.ssos;
+package com.android.internal.util.custom.cutout;
 
 import android.content.Context;
 
