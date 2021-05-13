7a1590a7a8c2539a930ce52c3141d2b6271a12d6
00day0 <therandomuser11@gmail.com>
2021-05-13 22:00:21 +0530

    SystemUI: Implement FOD icon animation (1/2)
    Reverse engineered from ExtendedUI-2.1-GAPPS-R-OFFICIAL-cheeseburger-20210102-0435
    
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5734,6 +5734,12 @@ public final class Settings {
         public static final String FOD_GESTURE = "fod_gesture";
 
         /**
+         * FOD icon animation
+         * @hide
+         */
+        public static final String FOD_ICON_ANIMATION = "fod_icon_animation";
+
+        /**
          * FOD pressed color
          * @hide
          */
