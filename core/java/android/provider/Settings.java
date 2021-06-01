ab2d68b88a09c45143c856b8a8e625fd8ed3a013
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-06-01 15:53:04 +0530

    overlays: Make Settings cards optional (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5929,10 +5929,15 @@ public final class Settings {
         public static final String LOCKSCREEN_BLUR = "lockscreen_blur";
 
         /**
-         * Wether auto brightness is applied one shot when screen is turned on
+         * Whether auto brightness is applied one shot when screen is turned on
          * @hide
          */
         public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
+
+        /**
+         * @hide
+         */
+        public static final String STYLE_OVERLAY_SETTINGS_CARDS = "style_overlay_settings_cards";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
