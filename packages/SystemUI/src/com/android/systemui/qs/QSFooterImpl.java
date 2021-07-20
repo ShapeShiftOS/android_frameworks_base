7a70585a287fb4bb536f886eb373305cdbf3a927
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-07-20 11:09:32 +0530

    QSPanel: Improve code
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -318,6 +318,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         post(() -> {
             updateVisibilities();
             updateClickabilities();
+            mQsPanel.setFooterPageIndicator(mPageIndicator);
             setClickable(false);
         });
     }
