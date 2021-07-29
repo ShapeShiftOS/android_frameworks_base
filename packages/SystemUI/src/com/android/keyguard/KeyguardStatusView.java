4bc8e2d2ecbf4fd1d5e2510804ff0844fbfc7c4a
mezzlasha <mezzlasha@gmail.com>
2021-07-29 15:12:02 +0530

    keyguard: Introduce Android 12 ShapeShift clock
    TikkiX2: Initial base
    jjpprrrr: Fix some crashes and add wallpaper color
    
    Mezz:
    Add font weight interpolation from ambient display to lockscreen
    Add font size interpolation from ambient display to lockscreen
    Add proper thumbnail
    Mimic font sizes, shadows, padding and thickness from A12
    
    Ashutosh:
    Fix some more crashes
    Fix owner info padding
    Add DND title for S clock in StatusView
    Fix uneven DND icon padding when using 12 clock
    Fix uneven music title when using 12 clock
    Smoothly animate from white color to wallpaper colored from ambient display to lockscreen
    
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Co-authored-by: TikkiTikki <radstylemusic@gmail.com>
    Co-authored-by: Chenyang Zhong <zhongcy95@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -323,7 +323,7 @@ public class KeyguardStatusView extends GridLayout implements
                 resolver, Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE);
             boolean mCustomClockSelectionType = currentClock == null ? false : currentClock.contains("Type");
             boolean mCustomClockSelectionIDE = currentClock == null ? false : currentClock.contains("IDE");
-            boolean mCustomClockSelectionShapeShift = currentClock == null ? false : currentClock.contains("ShapeShift");
+            boolean mCustomClockSelectionShapeShift = currentClock == null ? false : (currentClock.contains("ShapeShift") || currentClock.contains("Twelve"));
 
             // If left aligned style clock, align the textView to start else keep it center.
             if (mCustomClockSelectionType) {
--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -164,6 +164,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new AndroidTwelveClockController(res, layoutInflater, colorExtractor));
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
         mWidth = dm.widthPixels;
