b3eb7df458aedd5e166d7a3737aa7beafc7877ec
AshutoshSundresh <ashutoshsundresh@gmail.com>
2022-02-21 15:52:14 +0530

    VolumeDialog: Left align VolumeDialog for OnePlus devices by default
    Just for convenience

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -63,6 +63,7 @@ import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.RotateDrawable;
 import android.media.AudioManager;
 import android.media.AudioSystem;
+import android.os.Build;
 import android.os.Debug;
 import android.os.Handler;
 import android.os.Looper;
@@ -306,7 +307,8 @@ public class VolumeDialogImpl implements VolumeDialog,
         }
 
         initDimens();
-        mLeftVolumeRocker = mContext.getResources().getBoolean(R.bool.config_audioPanelOnLeftSide);
+        mLeftVolumeRocker = mContext.getResources().getBoolean(R.bool.config_audioPanelOnLeftSide)
+                || Build.MANUFACTURER.contains("OnePlus");
     }
 
     @Override
